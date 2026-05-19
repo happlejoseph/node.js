6a67851e1d7359b1d304363eb558efe422db5193
happlejoseph <vyshnavanil18753@gamil.com>
2026-05-19 13:38:51 +0530

    code modulaes fs.mkdir, fs.writefile, fs.readfile, fs.appendfile, fs.unlink, fs.rmdix, fs.exists syns, path badename, extname, dirname

---
+++ coreModules/fs/index.js
@@ -0,0 +1,102 @@
+
+
+const fs = require('fs'); // to import fs module //
+
+
+// // fs.readFile('sample.txt','utf8', (err,data)=> {
+// //     if(err) {
+// //         console.log('error: ', err);
+        
+// //     }
+// //     else {
+// //         console.log(data);
+        
+// //     }
+// // })
+
+
+// try {
+//     const data = fs.readFileSync('sample.txt','utf8');
+//     console.log(data);
+    
+// }
+// catch(err) {
+//     console.log('error: ', err);
+    
+// }
+
+
+
+// fs.mkdir('myFolder', (err)=> {
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log('folder created successfully');
+        
+//     }
+// });
+
+
+
+// fs.writeFile('myFolder/new.txt', 'helo node js', (err)=> { // fs.writeFile ('foldername/filename','content', (callback)=>{})
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log('file created successfully');
+        
+//     }
+// });
+
+
+// fs.readFile('myFolder/new.txt','utf8', (err,data)=> {
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log(data);
+        
+//     }
+// });
+
+
+// fs.appendFile('myFolder/new.txt','\nWelcome to node js', (err)=> {
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log('data append successfully');
+        
+//     }
+// });
+
+
+// fs.unlink('myFolder/new.txt', (err)=> {
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log('file deleted successfully');
+        
+//     }
+// });
+
+// fs.rmdir('myFolder', (err)=> {
+//     if (err) {
+//         console.log('error: ',err);
+        
+//     }
+//     else {
+//         console.log('delete folder successfully');
+        
+//     }
+// });
+
+
+
