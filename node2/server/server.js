715fdda502612675a72b4ea5687db8601a38f8db
happlejoseph <happlejoseph66@gmail.com>
2026-05-25 13:29:18 +0530

    server

---
+++ node2/server/server.js
@@ -0,0 +1,27 @@
+
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+
+const port = 3001
+
+const app = http.createServer((req,res)=> {
+    const {pathname} = url.parse(req.url);
+
+    if(pathname === '/') {
+        res.writeHead(200, {"content-type":'text/html'})
+        res.end(fs.readFileSync('../client/index.html'))
+    }
+    else if(pathname === '/js/index.js') {
+        res.writeHead(200, {"content-type":'text/html'})
+        res.end(fs.readFileSync('../client/js/index.js'))
+    }
+});
+
+app.listen(port,()=> {
+    console.log(`server is running on port ${port}`);
+    
+
+});
