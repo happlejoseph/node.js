402b7fdb98e3ff8f34167c64cdf220c326d038fe
happlejoseph <happlejoseph66@gmail.com>
2026-06-04 13:36:09 +0530

    read , write, duples

--- node2/stream/index.js
+++ node2/stream/index.js
@@ -7,10 +7,31 @@ const fs = require('fs');
     
 // });
 
-const stream = fs.createReadStream('video.mp4');
-console.log(stream);
+// const stream = fs.createReadStream('video.mp4');
+// console.log(stream);
 
-stream.on('data', (chunk)=> {
-    console.log('Recived chunk:', chunk.length);
+// stream.on('data', (chunk)=> {
+//     console.log('Recived chunk:', chunk.length);
+    
+// })
+
+
+
+
+// 1 Writaable Stream //
+const writeStream = fs.createWriteStream('sample.txt');
+writeStream.write('helo, this is a sample text\n')
+writeStream.write('helo, this is another line of text\n')
+writeStream.end('this is the ebd of the file')
+
+
+// 2 Readable Stream //
+const readStream = fs.createReadStream('sample.txt', 'utf-8')
+
+readStream.on('data', (chunk)=> {
+    console.log('Recived chunk:', chunk);
     
 })
+
+
+// 3 Duplex Stream //
